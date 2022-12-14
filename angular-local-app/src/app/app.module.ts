d137490f1a0adf35ae3efbec6ddcfd6efa0275e2
Hila Medina <hilam@moveo.co.il>
2022-12-14 12:46:24 +0200

    add comment

--- angular-local-app/src/app/app.module.ts
+++ angular-local-app/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
-
+//
 @NgModule({
 	declarations: [AppComponent, LoginComponent, DashboardComponent],
 	imports: [BrowserModule, AppRoutingModule, FormsModule],
